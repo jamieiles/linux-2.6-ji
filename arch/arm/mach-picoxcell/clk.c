b3251c1dccfef63ded6c4f7892816d13a8a26f2f
Jamie Iles <jamie@jamieiles.com>
2011-08-01 12:13:19 +0100

    picoxcell: add a binding for pc3x3 PLL's
    PC3X3 contains a PLL for the ARM CPU.  The PLL can be set to any
    frequency from 140MHz to 700MHz at 5MHz intervals.  Add a binding so
    that the PLL can be instantiated from the device tree.
    
    Signed-off-by: Jamie Iles <jamie@jamieiles.com>

--- arch/arm/mach-picoxcell/clk.c
+++ arch/arm/mach-picoxcell/clk.c
@@ -297,6 +297,173 @@ static void __init picoxcell_add_pc3x3_gated_clk(struct device_node *gate)
 	}
 }
 
+struct pc3x3_pll {
+	struct clk	clk;
+	void __iomem	*regs;
+	unsigned long	min_freq;
+	unsigned long	max_freq;
+};
+
+static inline struct pc3x3_pll *to_pc3x3_pll(struct clk *clk)
+{
+	return container_of(clk, struct pc3x3_pll, clk);
+}
+
+#define PC3X3_PLL_CLKF_REG_OFFS		0x00
+#define PC3X3_PLL_FREQ_SENSE_REG_OFFS	0x04
+#define PC3X3_PLL_FREQ_SENSE_VALID	(1 << 29)
+#define PC3X3_PLL_FREQ_SENSE_ACTIVE	(1 << 30)
+#define PC3X3_PLL_FREQ_SENSE_START	(1 << 31)
+#define PC3X3_PLL_FREQ_SENSE_FREQ_MASK	0x3FF
+#define PC3X3_PLL_STEP			5000000
+
+static unsigned long __pc3x3_pll_get_rate(struct clk *clk)
+{
+	unsigned int mhz = 0;
+	unsigned long sense_val;
+	struct pc3x3_pll *pll = to_pc3x3_pll(clk);
+
+	while (0 == mhz) {
+		do {
+			writel(PC3X3_PLL_FREQ_SENSE_START,
+			       pll->regs + PC3X3_PLL_FREQ_SENSE_REG_OFFS);
+
+			/* Wait for the frequency sense to complete. */
+			do {
+				sense_val =
+					readl(pll->regs +
+					      PC3X3_PLL_FREQ_SENSE_REG_OFFS);
+			} while ((sense_val & PC3X3_PLL_FREQ_SENSE_ACTIVE));
+		} while (!(sense_val & PC3X3_PLL_FREQ_SENSE_VALID));
+
+		/* The frequency sense returns the frequency in MHz. */
+		mhz = (sense_val & PC3X3_PLL_FREQ_SENSE_FREQ_MASK);
+	}
+
+	return mhz * 1000000;
+}
+
+static unsigned long pc3x3_pll_get_rate(struct clk *clk)
+{
+	unsigned long flags, rate;
+
+	spin_lock_irqsave(&clk_lock, flags);
+	rate = __pc3x3_pll_get_rate(clk);
+	spin_unlock_irqrestore(&clk_lock, flags);
+
+	return rate;
+}
+
+static long pc3x3_pll_round_rate(struct clk *clk, unsigned long rate)
+{
+	long ret;
+	unsigned long offset;
+	struct pc3x3_pll *pll = to_pc3x3_pll(clk);
+
+	rate = clamp(rate, pll->min_freq, pll->max_freq);
+	offset = rate % PC3X3_PLL_STEP;
+	rate -= offset;
+
+	if (offset > PC3X3_PLL_STEP - offset)
+		ret = rate + PC3X3_PLL_STEP;
+	else
+		ret = rate;
+
+	return ret;
+}
+
+static void pc3x3_pll_set(struct clk *clk, unsigned long rate)
+{
+	struct pc3x3_pll *pll = to_pc3x3_pll(clk);
+	unsigned long clkf = ((rate / 1000000) / 5) - 1;
+
+	writel(clkf, pll->regs + PC3X3_PLL_CLKF_REG_OFFS);
+	udelay(2);
+}
+
+static int pc3x3_pll_set_rate(struct clk *clk, unsigned long target)
+{
+	unsigned long flags, current_khz;
+
+	target = clk_round_rate(clk, target);
+	target /= 1000;
+
+	pr_debug("set cpu clock rate to %luKHz\n", target);
+
+	spin_lock_irqsave(&clk_lock, flags);
+
+	/*
+	 * We can only reliably step by 20% at a time. We may need to
+	 * do this in several iterations.
+	 */
+	while ((current_khz = __pc3x3_pll_get_rate(clk) / 1000) != target) {
+		unsigned long next_step, next_target;
+
+		if (target < current_khz) {
+			next_step = current_khz - ((4 * current_khz) / 5);
+			next_target = current_khz -
+				min(current_khz - target, next_step);
+			next_target = roundup(next_target * 1000,
+					      PC3X3_PLL_STEP);
+		} else {
+			next_step = ((6 * current_khz) / 5) - current_khz;
+			next_target =
+				min(target - current_khz, next_step) +
+				current_khz;
+			next_target = ((next_target * 1000) / PC3X3_PLL_STEP) *
+				PC3X3_PLL_STEP;
+		}
+
+		pc3x3_pll_set(clk, next_target);
+	}
+
+	spin_unlock_irqrestore(&clk_lock, flags);
+
+	return 0;
+}
+
+static const struct clk_ops pc3x3_pll_ops = {
+	.get_rate	= pc3x3_pll_get_rate,
+	.round_rate	= pc3x3_pll_round_rate,
+	.set_rate	= pc3x3_pll_set_rate,
+};
+
+static void __init picoxcell_add_pc3x3_pll(struct device_node *np)
+{
+	struct pc3x3_pll *clk;
+	u32 min, max;
+	void __iomem *regs;
+
+	if (of_property_read_u32(np, "picochip,min-freq", &min)) {
+		pr_err("no picochip,min-freq for %s\n", np->full_name);
+		return;
+	}
+
+	if (of_property_read_u32(np, "picochip,max-freq", &max)) {
+		pr_err("no picochip,max-freq for %s\n", np->full_name);
+		return;
+	}
+
+	regs = of_iomap(np, 0);
+	if (!regs) {
+		pr_err("unable to map regs for %s\n", np->full_name);
+		return;
+	}
+
+	clk = kzalloc(sizeof(*clk), GFP_KERNEL);
+	if (!clk)
+		panic("unable to allocate clk for %s\n", np->full_name);
+
+	clk->clk.ops = &pc3x3_pll_ops;
+	clk->clk.name = np->name;
+	clk->clk.of_node = np;
+	clk->regs = regs;
+	clk->min_freq = min;
+	clk->max_freq = max;
+
+	picoxcell_clk_add(&clk->clk);
+}
+
 static struct clk *picoxcell_find_clk(struct device_node *np)
 {
 	struct clk *clk;
@@ -339,6 +506,10 @@ static const struct of_device_id picoxcell_clk_match[] = {
 	{
 		.compatible = "picochip,pc3x3-clk-gate",
 		.data = picoxcell_add_pc3x3_gated_clk,
+	},
+	{
+		.compatible = "picochip,pc3x3-pll",
+		.data = picoxcell_add_pc3x3_pll,
 	},
 	{ /* Sentinel */ }
 };
