a43fed09532618dd3b2d1fce64a0028b9d159e7e
Russell King - ARM Linux <linux@arm.linux.org.uk>
2011-08-09 09:08:42 +0100

    ARM: gpio: make trivial GPIOLIB implementation the default
    Rather than marking the mach/gpio.h header files which want to use the
    trivial GPIOLIB implementation, mark those which do not want to use it
    instead.  This means that by default, you get the trivial implementation
    and only have to do something extra if you need to.  This should
    encourage the use of the trivial default implementation.
    
    As an additional bonus, several gpio.h header files become empty.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/plat-nomadik/include/plat/gpio.h
+++ arch/arm/plat-nomadik/include/plat/gpio.h
@@ -12,8 +12,6 @@
 #ifndef __ASM_PLAT_GPIO_H
 #define __ASM_PLAT_GPIO_H
 
-#define __ARM_GPIOLIB_TRIVIAL
-
 /*
  * "nmk_gpio" and "NMK_GPIO" stand for "Nomadik GPIO", leaving
  * the "gpio" namespace for generic and cross-machine functions
